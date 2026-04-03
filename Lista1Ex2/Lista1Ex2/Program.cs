803638e7a6b265927817329040318aa87259a990
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-02 22:33:14 -0300

    Add files via upload

---
+++ Lista1Ex2/Lista1Ex2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            int a;
+
+            Console.WriteLine("Informe a aresta do Quadrado:");
+            l = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("A área do quadrado é ");
+            a = l * l;
+            Console.WriteLine(a);
+
+        }
+    }
+}
