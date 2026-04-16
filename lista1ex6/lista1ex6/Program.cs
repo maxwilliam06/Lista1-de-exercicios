e143a33f973bdf02c2aa3867af04c45729d576ef
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:20:12 -0300

    Add files via upload

---
+++ lista1ex6/lista1ex6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double v1 = 0, v2 = 0;
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            v1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo valor: ");
+            v2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("A média geométrica dos valores é {0}", Math.Sqrt(v1 * v2));
+            Console.ReadLine();
+        }
+    }
+}
