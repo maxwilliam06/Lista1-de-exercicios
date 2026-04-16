1d8647e5ac830a4f381d6d54755d7b0f268b7b46
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:20:58 -0300

    Add files via upload

---
+++ lista1ex8/lista1ex8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite a temperatura em graus Celsius: ");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine("A temperatura em Fahrenheit é {0} graus Fahrenheit", (c * 1.9) + 32);
+            Console.ReadLine();
+        }
+    }
+}
