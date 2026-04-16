6d67abdadc1fe20a90c24de9621e18e034ee073f
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:20:36 -0300

    Add files via upload

---
+++ lista1ex7/lista1ex7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double m = 0;
+            double km = 0;
+
+
+            Console.WriteLine("Digite a distância em milhas marítimas: ");
+            m = double.Parse(Console.ReadLine());
+
+            km = m * 1.852;
+            Console.WriteLine("Resultado em quilômetros: {0}", km);
+            //1,852
+            Console.ReadLine();
+        }
+    }
+}
