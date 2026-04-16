8e690bc71c026ec91d37b01d3c377cd7cff58cc1
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:21:19 -0300

    Add files via upload

---
+++ lista1ex9/lista1ex9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double d;
+
+            Console.WriteLine("Digite o valor do diâmetro: ");
+            d = double.Parse(Console.ReadLine());
+
+            double r = d / 2;
+
+            Console.WriteLine("A área do circulo é {0}", (3.14 * (r * r)));
+
+            Console.ReadLine();
+        }
+    }
+}
