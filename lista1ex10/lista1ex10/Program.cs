391cb768c149369694ff3e19b2714aac84109116
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:21:47 -0300

    Add files via upload

---
+++ lista1ex10/lista1ex10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float c;
+            float v;
+
+            Console.WriteLine("Qual a cotação do dólar desejada: ");
+            c = float.Parse(Console.ReadLine());
+            Console.WriteLine("Qual a o valor de dólares: ");
+            v = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("A conversão será: {0} R$", c * v);
+            Console.ReadLine();
+        }
+    }
+}
