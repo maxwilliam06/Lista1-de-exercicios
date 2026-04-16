365b4e4d97726a8dc8741f46fe4e42077ca8d791
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:19:26 -0300

    Add files via upload

---
+++ lista1ex4/lista1ex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int b;
+            int h;
+
+            Console.WriteLine("Digite o valor da base do triângulo: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da altura do triângulo: ");
+            h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do triângulo é {0}", (b * h) / 2);
+            Console.ReadLine();
+        }
+    }
+}
