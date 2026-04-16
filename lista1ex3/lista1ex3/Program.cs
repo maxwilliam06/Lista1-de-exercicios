75107cdd480859456adc75fc74f236c0867c1b17
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:18:12 -0300

    Add files via upload

---
+++ lista1ex3/lista1ex3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int d;
+
+            Console.WriteLine("Digite o valor da diagonal do quadrado: ");
+            d = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("A área do quadrado é {0}", (d * d) / 2);
+            Console.ReadLine();
+        }
+    }
+}
