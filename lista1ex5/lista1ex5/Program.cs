a3bd9b726ba093ea04db0b8c1f02a8db29b47f38
maxwilliam06 <maxnascimento1706@gmail.com>
2026-04-16 15:19:45 -0300

    Add files via upload

---
+++ lista1ex5/lista1ex5/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 0, n2 = 0, n3 = 0, n4 = 0;
+            int i = 1;
+
+            while (i < 5)
+            {
+                double tempnota = 0;
+
+                Console.WriteLine("Digite a nota {0}: ", i);
+                tempnota = double.Parse(Console.ReadLine());
+                if (i == 1)
+                    n1 = tempnota;
+                else if (i == 2)
+                    n2 = tempnota;
+                else if (i == 3)
+                    n3 = tempnota;
+                else if (i == 4)
+                    n4 = tempnota;
+
+                i++;
+            }
+            ;
+            Console.WriteLine("A média é {0}", (n1 + n2 + n3 + n4) / 4);
+
+
+            Console.ReadLine();
+        }
+    }
+}
